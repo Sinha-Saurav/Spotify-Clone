59e9d5a8014c524c360e69ca6385ce3af2df27d3
Sinha-Saurav <sinha69saurav@gmail.com>
2025-02-10 13:48:11 +0530

    added a song library using JS and made a external song API

--- Spotify clone/script.js
+++ Spotify clone/script.js
@@ -1,13 +1,31 @@
 async function main(){
-    let songs = ["billie-eilish-birds-of-a-feather-official-music-video-128-ytshorts.savetube.me.mp3",
-        "bruno-mars-thats-what-i-like-official-music-video-128-ytshorts.savetube.me.mp3","bruno-mars-the-lazy-song-official-music-video-128-ytshorts.savetube.me.mp3","die-for-you-ft-grabbitz-official-music-video-valorant-champions-2021-128-ytshorts.savetube.me.mp3","fighter-mitti-full-video-hrithik-roshan-deepika-padukone-anil-kapoor-vishal-sheykhar-128-ytshorts.savetube.me.mp3","lady-gaga-bruno-mars-die-with-a-smile-official-music-video-128-ytshorts.savetube.me.mp3","one-direction-night-changes-128-ytshorts.savetube.me.mp3","ticking-away-ft-grabbitz-bbno-official-music-video-valorant-champions-2023-anthem-128-ytshorts.savetube.me.mp3","yung-kai-blue-official-music-video-128-ytshorts.savetube.me.mp3"];
-    let base_url = "http://127.0.0.1:5500/Spotify%20clone/assets/songs/";    
-
-    for(let song of songs){
+    let base_url = "http://127.0.0.1:5500/Spotify%20clone/assets/songs/";
+    let response = await fetch("http://127.0.0.1:5500/Spotify%20clone/assets/songs.json");
+    let data = await response.json();
+    let songs = data.songs;
+    let music = []
+    songs.forEach(song => {
         let url = base_url+song;
-        let response = await fetch(url);
-        let a = await response.text();
-        console.log(a);
-    }
+        music.push(url)
+    });
+    return music;
 }
-main()
+async function displaySongs() {
+    let music = await main();  // Wait for main() to finish
+    let SongUl = document.querySelector(".songsList ul");
+
+    music.forEach(mu => {
+        let songName = cleanSongName(mu.split('/').pop());  // Clean the song name
+        SongUl.innerHTML += `<li>${songName}</li>`;
+    });
+}
+
+function cleanSongName(rawName) {
+    return rawName
+    .replace(/-official-music-video.*|[-_]*\d{3}[-_ytshorts.savetube.me]*|\.mp3/g, "")  // Remove unwanted parts
+    .replace(/-/g, " ")  // Replace hyphens with spaces
+    .replace(/\s+/g, " ")  // Remove extra spaces
+    .trim()
+}
+
+displaySongs();
