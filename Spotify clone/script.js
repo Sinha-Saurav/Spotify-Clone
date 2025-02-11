67a3ae683401885b4202082e0f436e49c9d40b92
Sinha-Saurav <sinha69saurav@gmail.com>
2025-02-11 21:45:14 +0530

    made the playbar working except next and previous buttons

--- Spotify clone/script.js
+++ Spotify clone/script.js
@@ -11,21 +11,47 @@ async function main(){
     });
     return music;
 }
-const playMusic = (track)=>{
+const playMusic = (track , song)=>{
     currentSong.src = track;
     currentSong.play();
     play.src = "assets/logos/pause.svg";
+    document.querySelector(".songinfo").innerHTML = song;
 }
 
 async function displaySongs() {
     let music = await main();  // Wait for main() to finish
     let SongUl = document.querySelector(".songsList ul");
 
+    function secondsToMinutes(time){
+        let min = Math.floor(time/60);
+        let sec = Math.floor(time % 60);
+
+        min = String(min).padStart(2, '0');
+        sec = String(sec).padStart(2, '0');
+
+        return `${min}:${sec}`;
+    }
+
+    // 🔹 Auto-load first song on page load
+    if (music.length > 0) {
+        let firstSong = music[0];
+        let firstSongName = cleanSongName(firstSong.split('/').pop());
+
+        currentSong.src = firstSong;
+        document.querySelector(".songinfo").innerHTML = firstSongName;
+        document.querySelector(".songtime").innerHTML = "00:00 / 00:00"; // Reset time
+
+        // Once metadata is loaded, update duration
+        currentSong.addEventListener("loadedmetadata", () => {
+            document.querySelector(".songtime").innerHTML = `00:00 / ${secondsToMinutes(currentSong.duration)}`;
+        });
+    }
+
     //Show all the songs in playlist
 
     music.forEach((mu, index)=> {
-        let songName = cleanSongName(mu.split('/').pop());  // Clean the song name
-        SongUl.innerHTML += ` <li data-index="${index}">
+        const songName = cleanSongName(mu.split('/').pop());  // Clean the song name
+        SongUl.innerHTML += ` <li data-index="${index}", data-songname = "${songName}">
                             <img class="invert"src="assets/logos/music.svg" alt="">
                             <div class="info">
                                 <div>${songName}</div>
@@ -39,7 +65,8 @@ async function displaySongs() {
     document.querySelectorAll(".songsList li").forEach((element) => {
         element.addEventListener("click", (e) => {
             let index = element.getAttribute("data-index");  // Get song index
-            playMusic(music[index]);   // Play using full song URL
+            let songName = element.getAttribute("data-songname");  // Get song naem without url
+            playMusic(music[index],songName);   // Play using full song URL
         });
     });
 
@@ -52,6 +79,19 @@ async function displaySongs() {
             currentSong.pause()
             play.src = 'assets/logos/play.svg'
         }
+    })
+
+    //listen for timeupdate event
+    currentSong.addEventListener("timeupdate", e=>{
+        document.querySelector(".songtime").innerHTML = `${secondsToMinutes(currentSong.currentTime)} / ${secondsToMinutes(currentSong.duration)}`; 
+        document.querySelector(".circle").style.left = (currentSong.currentTime/currentSong.duration)*100 + "%";
+        document.querySelector(".progress").style.width = (currentSong.currentTime/currentSong.duration)*100 + "%";
+    })
+    //add a event listener to seekbar
+    document.querySelector(".seekbar").addEventListener("click", e=>{
+        let percent = (e.offsetX / e.target.getBoundingClientRect().width)*100
+        document.querySelector(".circle").style.left = percent + "%";
+        currentSong.currentTime = (currentSong.duration * percent)/100;
     })
 }
 
